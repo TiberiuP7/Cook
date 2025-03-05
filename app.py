fc56179bd702357b13c52d2d169ef9a84701658e
Tiberiu-Andrei Panghe <121564755+TiberiuP7@users.noreply.github.com>
2025-03-05 16:03:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 
 from datetime import datetime
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
